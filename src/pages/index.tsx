fa9fe42356f9104103d6a57d3b9283f60eff33c0
thiagosprestes <thiago.santos.prestes@gmail.com>
2021-02-24 22:30:13 -0300

    Aula 3 - Criado contexto da aplicação.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
+import Head from "next/head";
+
 import CompletedChallenges from "../components/CompletedChallenges";
+import ChallengeBox from "../components/ChallengeBox";
 import Countdown from "../components/Countdown";
 import ExperienceBar from "../components/ExperienceBar";
 import Profile from "../components/Profile";
-
-import Head from "next/head";
 
 import styles from "../styles/pages/Home.module.css";
 
@@ -22,7 +23,9 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   );
